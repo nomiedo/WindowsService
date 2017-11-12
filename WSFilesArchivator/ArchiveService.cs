edf7d1d0a850c258f1768703524ccd9d5226a984
aleksand zareckij <zareckij.aleksandr@gmail.com>
2017-11-12 19:50:42 +0300

    add tasks

--- WSFilesArchivator/ArchiveService.cs
+++ WSFilesArchivator/ArchiveService.cs
@@ -4,44 +4,78 @@ using System.Threading;
 using System.IO.Compression;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace WSFilesArchivator
 {
     public class ArchiveService
     {
-
         private readonly Timer timer;  
         private readonly string resourceFolderPath;
         private readonly string resultFolderPath;
-
-        private Regex re = new Regex(@"([a-zA-Z]+)(\d+)");
+        private readonly string wrongFolderPath;
+        private Regex re = new Regex(@"(^IMG_[0-9][0-9][0-9].[jpg])\w+");
+        List<FileInfo> batch = new List<FileInfo>();
+        Task currentTask;
 
         public ArchiveService()
         {
-            timer = new Timer(Archivation);
-            resourceFolderPath = @"e:\Mentoring\WindowsService\WSFilesArchivator\bin\Debug\ResourceFolder\";
-            resultFolderPath = @"e:\Mentoring\WindowsService\WSFilesArchivator\bin\Debug\ResultFolder\";
+            timer = new Timer(Handle);
+            resourceFolderPath = System.Configuration.ConfigurationManager.AppSettings["ResourceFilePath"];
+            resultFolderPath = System.Configuration.ConfigurationManager.AppSettings["ResultFilePath"];
+            wrongFolderPath = System.Configuration.ConfigurationManager.AppSettings["WrongFilePath"];
         }
 
         public bool Start()
         {
-            timer.Change(0, 300000);
+            timer.Change(0, 3000);
+
+
             return true;
         }
 
         public bool Stop()
         {
+            currentTask.Wait();
             timer.Change(Timeout.Infinite, 0);
             return true;
         }
 
-        public void Archivation(object target)
+        static object _sync = new object();
+
+        private void Handle(object target)
+        {
+            lock (_sync)
+            {
+                currentTask = Task.Factory.StartNew(() =>
+                {
+                    int count = Archivation();
+                    if (count == -1) { Console.WriteLine(@"Corrupted file was detected"); }
+                    if (count == 0) { Console.WriteLine(@"There are no files to create archive"); }
+                    if (count > 0) { CreateArchive(batch); }
+                });
+            }
+        }
+
+        private int Archivation()
         {
             DirectoryInfo resourceDirectory = new DirectoryInfo(resourceFolderPath);
-            List<FileInfo> batch = new List<FileInfo>();
+
+            if (!resourceDirectory.Exists)
+                return -1;
+
+            if (resourceDirectory.GetFiles("*.jpg").Length < 1)
+                return 0;
+
             int fileIndex = 0;
             foreach (var file in resourceDirectory.GetFiles("*.jpg"))
             {
+                if (!re.IsMatch(file.Name))
+                {
+                    file.MoveTo(wrongFolderPath + file.Name);
+                    continue;
+                }
+                
                 // remove extension from the name
                 string name = file.Name.Substring(0, file.Name.Length - 4);
                 // get a number
@@ -49,22 +83,19 @@ namespace WSFilesArchivator
                 string number = file.Name.Substring(a, name.Length - a);
                 row = Convert.ToInt32(number);
 
-                if (row - fileIndex == 1 || fileIndex == 0)
-                {
-                    batch.Add(file);
-                }
-                else
+                if (row - fileIndex != 1 && fileIndex > 0)
                 {
                     CreateArchive(batch);
                     batch.Clear();
-                    batch.Add(file);
                 }
+
+                batch.Add(file);
                 fileIndex = row;
             }
-            CreateArchive(batch);
+            return 1;
         }
 
-        public void CreateArchive(List<FileInfo> files)
+        private void CreateArchive(List<FileInfo> files)
         {
             string zipName = $@"{resultFolderPath}{DateTime.Now.ToString("yyyyMMdd_hhmmss")}.zip";
             using (ZipArchive newFile = ZipFile.Open(zipName, ZipArchiveMode.Create))
@@ -73,6 +104,10 @@ namespace WSFilesArchivator
                 {
                     newFile.CreateEntryFromFile(file.FullName, file.Name);
                 }
+            }
+            foreach (var file in files)
+            {
+                file.Delete();
             }
         }
 
