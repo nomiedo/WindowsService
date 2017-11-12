59ad49f693773f579b4b256f1c15e55a4c2a7715
aleksand zareckij <zareckij.aleksandr@gmail.com>
2017-11-12 17:15:17 +0300

    Simply Service

---
+++ WSFilesArchivator/ArchiveService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.IO.Compression;
+
+namespace WSFilesArchivator
+{
+    public class ArchiveService
+    {
+        
+        private readonly Timer timer;
+        private readonly string resourceFolderPath;
+        private readonly string resultFolderPath;
+
+        public ArchiveService()
+        {
+            timer = new Timer(Archivation);
+            resourceFolderPath = @"e:\Mentoring\WindowsService\WSFilesArchivator\bin\Debug\ResourceFolder\";
+            resultFolderPath = @"e:\Mentoring\WindowsService\WSFilesArchivator\bin\Debug\ResultFolder\";
+        }
+
+        public bool Start()
+        {
+            timer.Change(0, 5000);
+            return true;
+        }
+
+        public bool Stop()
+        {
+            timer.Change(Timeout.Infinite, 0);
+            return true;
+        }
+
+        public void Archivation(object target)
+        {
+            string zipName = $@"{resultFolderPath}{DateTime.Now.ToString("yyyyMMdd_hhmmss")}.zip";
+            string filepath1 = $@"{resourceFolderPath}IMG_001.jpg";
+            string filepath2 = $@"{resourceFolderPath}IMG_001.jpg";
+
+            using (ZipArchive newFile = ZipFile.Open(zipName, ZipArchiveMode.Create))
+            {
+                newFile.CreateEntryFromFile(filepath1, "IMG_001.jpg");
+                newFile.CreateEntryFromFile(filepath2, "IMG_002.jpg");
+            }
+        } 
+    }
+}
---
+++ WSFilesArchivator/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Topshelf;
+
+namespace WSFilesArchivator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<ArchiveService>(conf =>
+                {
+                    conf.ConstructUsing(() => new ArchiveService());
+                    conf.WhenStarted(s => s.Start());
+                    conf.WhenStopped(s => s.Stop());
+                });
+                x.StartAutomaticallyDelayed();
+                x.RunAsLocalService();
+                x.EnableServiceRecovery(r => r.RestartService(0).RestartService(1));
+            }); 
+        }
+    }
+}
