52edf80321f50c8315e5a200cf516f769cb4272c
aleksand zareckij <zareckij.aleksandr@gmail.com>
2017-11-12 18:01:30 +0300

    Add handling of files

--- WSFilesArchivator/ArchiveService.cs
+++ WSFilesArchivator/ArchiveService.cs
@@ -2,15 +2,19 @@
 using System.IO;
 using System.Threading;
 using System.IO.Compression;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace WSFilesArchivator
 {
     public class ArchiveService
     {
-        
-        private readonly Timer timer;
+
+        private readonly Timer timer;  
         private readonly string resourceFolderPath;
         private readonly string resultFolderPath;
+
+        private Regex re = new Regex(@"([a-zA-Z]+)(\d+)");
 
         public ArchiveService()
         {
@@ -21,7 +25,7 @@ namespace WSFilesArchivator
 
         public bool Start()
         {
-            timer.Change(0, 5000);
+            timer.Change(0, 300000);
             return true;
         }
 
@@ -33,15 +37,57 @@ namespace WSFilesArchivator
 
         public void Archivation(object target)
         {
-            string zipName = $@"{resultFolderPath}{DateTime.Now.ToString("yyyyMMdd_hhmmss")}.zip";
-            string filepath1 = $@"{resourceFolderPath}IMG_001.jpg";
-            string filepath2 = $@"{resourceFolderPath}IMG_001.jpg";
+            DirectoryInfo resourceDirectory = new DirectoryInfo(resourceFolderPath);
+            List<FileInfo> batch = new List<FileInfo>();
+            int fileIndex = 0;
+            foreach (var file in resourceDirectory.GetFiles("*.jpg"))
+            {
+                // remove extension from the name
+                string name = file.Name.Substring(0, file.Name.Length - 4);
+                // get a number
+                int row, a = getIndexofNumber(name);
+                string number = file.Name.Substring(a, name.Length - a);
+                row = Convert.ToInt32(number);
 
+                if (row - fileIndex == 1 || fileIndex == 0)
+                {
+                    batch.Add(file);
+                }
+                else
+                {
+                    CreateArchive(batch);
+                    batch.Clear();
+                    batch.Add(file);
+                }
+                fileIndex = row;
+            }
+            CreateArchive(batch);
+        }
+
+        public void CreateArchive(List<FileInfo> files)
+        {
+            string zipName = $@"{resultFolderPath}{DateTime.Now.ToString("yyyyMMdd_hhmmss")}.zip";
             using (ZipArchive newFile = ZipFile.Open(zipName, ZipArchiveMode.Create))
             {
-                newFile.CreateEntryFromFile(filepath1, "IMG_001.jpg");
-                newFile.CreateEntryFromFile(filepath2, "IMG_002.jpg");
+                foreach (var file in files)
+                {
+                    newFile.CreateEntryFromFile(file.FullName, file.Name);
+                }
             }
-        } 
+        }
+
+        private int getIndexofNumber(string cell)
+        {
+            int indexofNum = -1;
+            foreach (char c in cell)
+            {
+                indexofNum++;
+                if (Char.IsDigit(c))
+                {
+                    return indexofNum;
+                }
+            }
+            return indexofNum;
+        }
     }
 }
